0cf1e9e80372f3cdf9ebb6ffd249d97621980e8e
Ahmed Shaaban & Max Robinson <pair+AhmedShab.Maxwell-Robinson@devbootcamp.com>
2016-07-29 13:58:33 +1200

    idArray made

--- routes/index.js
+++ routes/index.js
@@ -9,18 +9,45 @@ var flickr = require('../config').flickr
 /* GET home page. */
 
 router.get('/', function(req, res, next) {
-    console.log("Getting the request");
-  // res.render('index', { title: 'Flickr Clickr' });
+  var puppyArray = []
+  //console.log("Getting the request");
+  res.render('index', { title: 'Flickr Clickr' });
   flickr
     .request()
     .media()
     .search("puppies")
-    .get()
+    .get({
+      page: 1,
+      per_page: 15
+    })
     .then(function (response) {
     // An array of media objects matching the search term
-    console.log(response);
+    //console.log(response);
+   puppyArray.push(response.body.photos.photo)
+    var idArray = puppyArray[0].map(function(photo){
+    return photo.id
+   })
+   console.log(idArray)
+
+
+
+
+  // flickr
+  //   .request()
+  //   .media('22397283330')
+  //   .get()
+  //   .then(function (response) {
+  //       // Photo object with links to resources
+  //   });
 });
+
+
 
 });
 
 module.exports = router;
+
+//access orject and retrieve the photo array.
+//take the array, pull one photo out of the array, and display.
+//
+//link button with a function which moves through the array and displays another function //
