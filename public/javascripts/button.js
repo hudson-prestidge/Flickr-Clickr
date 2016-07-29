0c754166f6ae941024cf413871c08848af6cde0d
Amaan & Hudson Burns Prestidge <pair+amaanvaddadi.hudson-prestidge@devbootcamp.com>
2016-07-29 13:52:14 +1200

    set up button function

---
+++ public/javascripts/button.js
@@ -0,0 +1,11 @@
+var db = require('../db.json')
+
+document.addEventListener('DOMContentLoaded', start)
+
+function start() {
+
+  var button = document.getElementById('clickr')
+
+  button.addEventListener('click', function() {
+  })
+}
