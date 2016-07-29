8b5e70ac9f02952223be7fc5484b5217efb472ce
Ahmed Shaaban & Max Robinson <pair+AhmedShab.Maxwell-Robinson@devbootcamp.com>
2016-07-29 14:50:49 +1200

    button calls a function

---
+++ public/javascripts/button.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', start)
+
+function start() {
+  var button = document.getElementById('clickr')
+  button.addEventListener('click', function() {
+    console.log('test')
+
+  })
+}
