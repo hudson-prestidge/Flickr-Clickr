59e59d52edd1f8578918866368b1785d728e4237
Max Robinson <Maxwell.robinson@hotmail.com>
2016-08-03 15:15:08 +1200

    app working perfectly

--- public/javascripts/button.js
+++ public/javascripts/button.js
@@ -5,7 +5,6 @@ function start() {
   var urlsArray = document.getElementById('image').dataset.allImage.split(',')
   button.addEventListener('click', function() {
     //pick a random image out of the array
-    //
     var index = Math.floor(Math.random()*urlsArray.length)
     document.getElementById('image').src = urlsArray[index]
   })
